54a5e97edb0acc3ed6ffd087d37242a013951a74
Luke Nguyen <github@wooferine.tech>
2021-11-06 23:38:20 -0400

    Landing page

--- server.js
+++ server.js
@@ -1,63 +1,69 @@
-"use strict";
+'use strict';
 
 const PORT = process.env.PORT || 8080;
 
-import cors from "cors";
-import express from "express";
-import { addUser, findUser } from "./Auth/database.js";
-import { getData } from "./Suggestion/database.js";
+import cors from 'cors';
+import express from 'express';
+import { addUser, findUser } from './Auth/database.js';
+import { getData } from './Suggestion/database.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/signup", (req, res) => {
-  console.log(req.body);
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(400).send({ message: "Missing username or password" });
-  } else {
-    if (addUser(username, password)) {
-      res.status(200).send({ message: "User created" });
-    } else {
-      res.status(400).send({ message: "User already exists" });
-    }
-  }
+app.use(express.static('public'));
+
+// Landing
+
+app.get('/', (req, res) => {
+  res.sendFile('landing.html', { root: './html' });
 });
 
-app.get("/messages", (req, res) => {
-  res.sendFile("messages.html", { root: "." });
-});
+// Login
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    res.status(400).send({ message: "Missing username or password" });
+    res.status(400).send({ message: 'Missing username or password' });
   } else {
     const user = findUser(username, password);
     if (user) {
-      res.status(200).send({ message: "User logged in", user });
+      res.status(200).send({ message: 'User logged in', user });
     } else {
-      res.status(400).send({ message: "Invalid username or password" });
+      res.status(400).send({ message: 'Invalid username or password' });
     }
   }
 });
 
-app.get("/suggestion", (req, res) => {
+// Signup
+
+app.post('/signup', (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).send({ message: 'Missing username or password' });
+  } else {
+    if (addUser(username, password)) {
+      res.status(200).send({ message: 'User created' });
+    } else {
+      res.status(400).send({ message: 'User already exists' });
+    }
+  }
+});
+
+// Suggestions
+
+app.get('/suggestion', (req, res) => {
   const userId = req.query.userId;
   if (!userId) {
-    res.status(400).send("Missing userId");
+    res.status(400).send('Missing userId');
     return;
   }
 
   res.status(200).send(getData(userId));
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
