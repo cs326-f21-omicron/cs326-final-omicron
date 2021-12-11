acc65de54712cdf01026a584eb018d7530d9ba8a
Luke Nguyen <github@wooferine.tech>
2021-12-11 15:46:29 -0500

    Update Home UI

--- js/home.js
+++ js/home.js
@@ -1,44 +1,45 @@
 const loadSuggestion = async () => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    let requestUrl = '/suggestion';
-    if (urlParams.has('category')) {
-        requestUrl += `?category=${urlParams.get('category')}`;
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  let requestUrl = '/suggestion';
+  if (urlParams.has('category')) {
+    requestUrl += `?category=${urlParams.get('category')}`;
+  }
+  try {
+    const res = await fetch(requestUrl, {
+      method: 'GET',
+    });
+
+    if (res.status === 400) {
+      throw new Error('Bad Request');
     }
-    try {
-        const res = await fetch(requestUrl, {
-            method: 'GET',
-        });
 
-        if (res.status === 400) {
-            throw new Error('Bad Request');
-        }
-
-        const data = await res.json();
-        let innterHtml = '';
-        console.log(data.length);
-        if (data.length !== 0) {
-            for (let i = 0; i < data.length; i++) {
-                const suggestion = data[i];
-                const posts = suggestion.posts;
-                const categoryTitle =
-                    suggestion.title.charAt(0).toUpperCase() +
-                    suggestion.title.slice(1);
-                const suggestionDiv = document.createElement('div');
-                suggestionDiv.classList.add('px-4');
-                innerHtml = `
+    const data = await res.json();
+    let innerHtml = '';
+    console.log(data.length);
+    if (data.length !== 0) {
+      for (let i = 0; i < data.length; i++) {
+        const suggestion = data[i];
+        const posts = suggestion.posts;
+        const categoryTitle =
+          suggestion.title.charAt(0).toUpperCase() +
+          suggestion.title.slice(1);
+        const suggestionDiv = document.createElement('div');
+        suggestionDiv.classList.add('px-4');
+        suggestionDiv.classList.add('pb-5');
+        innerHtml = `
       <h2 class="pb-2">${categoryTitle}</h2>
       <div
         class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 pt-3"
       >
         ${posts
             .map((post) => {
-                //calculate how many days ago the post was created
-                const date = new Date(post.date);
-                const now = new Date();
-                const diffTime = Math.abs(now - date);
-                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-                return `
+              //calculate how many days ago the post was created
+              const date = new Date(post.date);
+              const now = new Date();
+              const diffTime = Math.abs(now - date);
+              const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+              return `
               <a class="col nounderline" href="/view?id=${post._id}">
               <div
                 class="
@@ -52,7 +53,10 @@ const loadSuggestion = async () => {
                   custom-img
                   border-0
                 "
-                style="background-image: url(${post.image});"
+                style="background-image: linear-gradient(
+                  rgba(0, 0, 0, 0.5), 
+                  rgba(0, 0, 0, 0.5)
+                ), url(${post.image});"
               >
                 <div
                   class="
@@ -68,25 +72,12 @@ const loadSuggestion = async () => {
                     ${post.title}
                   </h2>
                   <ul class="d-flex list-unstyled mt-auto">
-                    <li class="me-auto">
-                      <img
-                        src= "https://avatar-prod-us-east-2.webexcontent.com/Avtr~V1~6078fba4-49d9-4291-9f7b-80116aab6974/V1~8d90dcca-87c5-4a7c-9808-8965a02be966~07b1fecf53a948688dabf01402621a0a"
-                        alt="Bootstrap"
-                        width="32"
-                        height="32"
-                        class="rounded-circle border border-white"
-                      />
-                    </li>
-                    <li class="d-flex align-items-center me-3">
-                      <svg class="bi me-2" width="1em" height="1em">
-                        <use xlink:href="#geo-fill" />
-                      </svg>
-                      <small>${post.location}</small>
-                    </li>
+                      <li class="d-flex align-items-center me-4">
+                        <i class="bi bi-pin-map me-2"></i>
+                        <small>${post.location}</small>
+                      </li>
                       <li class="d-flex align-items-center">
-                        <svg class="bi me-2" width="1em" height="1em">
-                          <use xlink:href="#calendar3" />
-                        </svg>
+                        <i class="bi bi-clock me-2"></i>
                         <small>${diffDays}d</small>
                       </li>
                     </ul>
@@ -97,26 +88,26 @@ const loadSuggestion = async () => {
             })
             .join('')}
           </div>
-          </div>   
+        </div>   
 `;
-                if (!urlParams.has('category')) {
-                    innerHtml += `<h5 class="text-end mt-3">
+        if (!urlParams.has('category')) {
+          innerHtml += `<h5 class="text-end mt-4">
         <a href="/home?category=${data[i]._id}" class="text-decoration-none">More postings</a>
       </h5>`;
-                }
-                suggestionDiv.innerHTML = innerHtml;
-                document
-                    .getElementById('suggestion')
-                    .appendChild(suggestionDiv);
-            }
-        } else {
-            innerHtml = `<h2 class="pb-2">Please add your hobbies to see your suggestion</h2>`;
-            document.getElementById('suggestion').innerHTML = innerHtml;
         }
-    } catch (err) {
-        console.log(err);
-        window.location.href = '/login';
+        suggestionDiv.innerHTML = innerHtml;
+        document
+          .getElementById('suggestion')
+          .appendChild(suggestionDiv);
+      }
+    } else {
+      innerHtml = `<h2 class="pb-2">Please add your hobbies to see your suggestion</h2>`;
+      document.getElementById('suggestion').innerHTML = innerHtml;
     }
+  } catch (err) {
+    console.log(err);
+    window.location.href = '/login';
+  }
 };
 
 window.addEventListener('load', loadSuggestion);
